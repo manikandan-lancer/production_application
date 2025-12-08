3afc8656354547847e5bee396e7d149d5c02e124
manikandan-lancer <manikandan@lancers.in>
2025-12-08 14:45:08 +0530

    Fixed Connection issues

--- database/connection.py
+++ database/connection.py
@@ -1,12 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-import os
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise Exception("DATABASE_URL environment variable missing!")
+DATABASE_URL = "postgresql://...YOUR DATABASE URL..."
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
-SessionLocal = sessionmaker(bind=engine)
+
+# ----------------------------
+# FIX: Add get_session function
+# ----------------------------
+def get_session():
+    return SessionLocal()
